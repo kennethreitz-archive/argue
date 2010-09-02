a6205b41fd0edb57c05cc4924e41ef104af27b96
Kenneth Reitz <me@kennethreitz.com>
2010-09-02 03:23:36 -0400

    PyPi setup.

--- argue/__init__.py
+++ argue/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from core import *
+from core import __version__
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-import gistapi
+import argue
 
 from distutils.core import setup
 
